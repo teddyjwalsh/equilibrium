02602f380708f01a67bef5604b9ab972e5886b0b
teddyjwalsh <teddyjwalsh@gmail.com>
2021-06-29 12:20:57 -0500

    Update action_system.h

--- action_system.h
+++ action_system.h
@@ -13,6 +13,7 @@ public:
   virtual void update(double dt) override
   {
     auto& action_comps = get_array<CompAction>();
+    auto& height_map = get_array<CompHeightMap>()[0];
     for (auto& action_comp : action_comps)
     {
       auto movement_comp = action_comp.sibling<CompMovement>();
@@ -24,9 +25,11 @@ public:
         if (position_comp && movement_comp)
         {
           auto dist_vec = cur_action.location - position_comp.pos;
+          dist_vec.z = 0;
           if (dist_vec.length() > 2)
           {
             glm::vec3 direction = glm::normalize();
+            // Check here for height of movement, account for this in movement speed and energy used
             position_comp.pos += direction*movement_comp.max_speed;
           }
           else
