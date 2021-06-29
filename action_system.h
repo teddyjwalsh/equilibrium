684ca66aa87ee1fbd3e6d837145b4e62e83940d1
teddyjwalsh <teddyjwalsh@gmail.com>
2021-06-29 12:15:52 -0500

    Update action_system.h
    Added some movement logic

--- action_system.h
+++ action_system.h
@@ -15,10 +15,25 @@ public:
     auto& action_comps = get_array<CompAction>();
     for (auto& action_comp : action_comps)
     {
-      Action& cur_action = action_comp.back();
+      auto movement_comp = action_comp.sibling<CompMovement>();
+      auto position_comp = action_comp.sibling<CompPosition>();
+      Action& cur_action = action_comp.queue.back();
       switch(cur_action.action_type)
       {
       case Action::ActionType::MOVE:
+        if (position_comp && movement_comp)
+        {
+          auto dist_vec = cur_action.location - position_comp.pos;
+          if (dist_vec.length() > 2)
+          {
+            glm::vec3 direction = glm::normalize();
+            position_comp.pos += direction*movement_comp.max_speed;
+          }
+          else
+          {
+            action_comp.queue.pop_back();
+          }
+        }
         break;
       default:
         break;
